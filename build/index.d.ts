f6fa69cd53d0df63a266649e7b7ee769bf625d09
Alex // timetocode <alex@timetocode.com>
2024-08-18 16:37:53 -0700

    support for node 22

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,4 +1,3 @@
-/// <reference types="node" />
 import { Buffer } from 'buffer';
 import { IServerNetworkAdapter, User, InstanceNetwork } from 'nengi';
 import { BufferReader, BufferWriter } from 'nengi-buffers';
